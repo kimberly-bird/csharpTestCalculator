5af9457aa7033977c947b9cb416f10a52bad058b
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-01-16 12:11:20 -0600

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    public class Calculator
+    {
+        public int x { get; set; } = 0;
+        public int y { get; set; } = 0;
+        public int AddTwoIntegers(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+    }
+    
+}
